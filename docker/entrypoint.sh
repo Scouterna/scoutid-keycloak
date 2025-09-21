1ae736317e4b74a7c22689cb954ff932b0e7f823
Malcolm Nihlén <malcolm.nihlen@gmail.com>
2025-09-21 20:02:51 +0200

    fix: move docker to subdir and fix build setting

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo Custom entrypoint start
-
 # Replace APPSETTING_KC_ prefix from Azure App Service environment variables
 # with KC_ prefix for Keycloak configuration.
 for var in $(env | grep "APPSETTING_KC_"); do
@@ -22,6 +20,15 @@ for var in $(env | grep "APPSETTING_KC_"); do
     echo "Set $new_var_name to value of $var_name"
   fi
 done
+
+# Fetch an access token for Azure Database for PostgreSQL from the Azure
+# Instance Metadata Service. The JDBC driver will use this token as the password
+export KC_DB_PASSWORD=$(curl 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fossrdbms-aad.database.windows.net' -H Metadata:true -s | jq -r '.access_token')
+
+if [ -z "$KC_DB_PASSWORD" ]; then
+  echo "Failed to get database access token."
+  exit 1
+fi
 
 # Run the 'exec' command as the last step of the script.
 # As it replaces the current shell process, no additional shell commands will run after the 'exec' command.
