65fda381c862596e33956ba6947f9f5fa5294c7b
Malcolm Nihlén <malcolm.nihlen@gmail.com>
2025-09-21 20:57:26 +0200

    fix: remove excessive logging

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -21,29 +21,14 @@ for var in $(env | grep "APPSETTING_KC_"); do
   fi
 done
 
+# Fetch an access token for Azure Database for PostgreSQL from the Azure
+# Instance Metadata Service. The JDBC driver will use this token as the password
 RESOURCE_URI="https://ossrdbms-aad.database.windows.net"
 TOKEN_AUTH_URI="${IDENTITY_ENDPOINT}?resource=${RESOURCE_URI}&api-version=2019-08-01"
 
-# Using curl to fetch the token and jq to extract the access_token
-ACCESS_TOKEN=$(curl -s -X GET \
+export KC_DB_PASSWORD=$(curl -s -X GET \
   -H "X-IDENTITY-HEADER: $IDENTITY_HEADER" \
   "$TOKEN_AUTH_URI" | jq -r '.access_token')
-
-echo "Resource URI: $RESOURCE_URI"
-echo "Token Auth URI: $TOKEN_AUTH_URI"
-echo "Command used: curl -s -X GET -H \"X-IDENTITY-HEADER: \$IDENTITY_HEADER\" \"\$TOKEN_AUTH_URI\" | jq -r '.access_token'"
-echo "Access Token: $ACCESS_TOKEN"
-
-export KC_DB_PASSWORD=$ACCESS_TOKEN
-
-# # Fetch an access token for Azure Database for PostgreSQL from the Azure
-# # Instance Metadata Service. The JDBC driver will use this token as the password
-# export KC_DB_PASSWORD=$(curl 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fossrdbms-aad.database.windows.net' -H Metadata:true -s | jq -r '.access_token')
-
-# if [ -z "$KC_DB_PASSWORD" ]; then
-#   echo "Failed to get database access token."
-#   exit 1
-# fi
 
 # Run the 'exec' command as the last step of the script.
 # As it replaces the current shell process, no additional shell commands will run after the 'exec' command.
